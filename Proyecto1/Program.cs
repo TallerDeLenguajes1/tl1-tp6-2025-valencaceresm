85f6a3f8d9c035718f5d991ce49eb7ec77553c6c
valencaceresm <caceresmendoza.valentina@gmail.com>
2025-05-20 15:36:25 -0300

    ejercicio 1 realizado

---
+++ Proyecto1/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+int a;
+int b;
+a= 10;
+b = a;
+int numeroInvertido = 0;
+Console.WriteLine("valor de a:"+a);
+Console.WriteLine("valor de b:" + b);
+
+Console.WriteLine("Ingrese un numero que quiera invertir");
+string entrada = Console.ReadLine();
+int numero;
+bool esNumero = int.TryParse(entrada, out numero);
+if (esNumero)
+{
+    Console.WriteLine($"El número ingresado es: {numero}");
+    while (numero > 0) {
+        int digito = numero % 10;
+        numeroInvertido = (numeroInvertido * 10) + digito;
+        numero /= 10;
+    }
+}
+else
+{
+    Console.WriteLine("La entrada no es un número entero válido.");
+}
+Console.WriteLine("El numero invertido es:"+numeroInvertido);
