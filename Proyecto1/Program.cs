b50145e7bfdc84711258f388fe45c6111af24bdd
valencaceresm <caceresmendoza.valentina@gmail.com>
2025-05-20 16:42:31 -0300

    agrego modificaciones

--- Proyecto1/Program.cs
+++ Proyecto1/Program.cs
@@ -5,6 +5,11 @@ int a;
 int b;
 a= 10;
 b = a;
+
+int eleccion;
+int nEleccion;
+float result;
+
 int numeroInvertido = 0;
 Console.WriteLine("valor de a:"+a);
 Console.WriteLine("valor de b:" + b);
@@ -26,4 +31,57 @@ else
 {
     Console.WriteLine("La entrada no es un número entero válido.");
 }
-Console.WriteLine("El numero invertido es:"+numeroInvertido);
+Console.WriteLine("El numero invertido es:" + numeroInvertido);
+
+do
+{
+    Console.WriteLine("Ingrese el primer numero con el que quiere trabajar");
+    string y = Console.ReadLine();
+    int numeroy;
+    bool esNumeroy = int.TryParse(y, out numeroy);
+
+    Console.WriteLine("Ingrese el segundo numero con el que quiera trabajar");
+    string x = Console.ReadLine();
+    int numerox;
+    bool esNumerox = int.TryParse(x, out numerox);
+
+    Console.WriteLine("Ingrese la operacion que quiere realizar:");
+    Console.WriteLine("1) Suma");
+    Console.WriteLine("2) Resta");
+    Console.WriteLine("3) Multiplicacion");
+    Console.WriteLine("4) Division");
+    Console.WriteLine("0) Salir");
+    string choice = Console.ReadLine();
+    bool esNumeroEleccion = int.TryParse(choice, out eleccion);
+
+    switch (eleccion)
+    {
+        case 1:
+            result = numeroy + numerox;
+            Console.WriteLine($"Resultado: {numeroy} + {numerox} = {result}");
+            break;
+        case 2:
+            result = numeroy - numerox;
+            Console.WriteLine($"Resultado: {numeroy} - {numerox} = {result}");
+            break;
+        case 3:
+            result = numeroy * numerox;
+            Console.WriteLine($"Resultado: {numeroy} x {numerox} = {result}");
+            break;
+        case 4:
+            if (numerox == 0)
+            {
+                Console.WriteLine("Error: División por cero no permitida.");
+            }
+            else
+            {
+                result = numeroy / numerox;
+                Console.WriteLine($"Resultado: {numeroy} / {x} = {result}");
+            }
+            break;
+    }
+    Console.WriteLine("Quiere seguir? 1)- Si 0)- No");
+    choice = Console.ReadLine();
+    bool nuevaEleccion = int.TryParse(choice, out nEleccion);
+    eleccion = nEleccion;
+} while (eleccion != 0);
